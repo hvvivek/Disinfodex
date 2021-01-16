54542284cc6314585601343490a3ed2b59dc91ba
hvvivek <hvvivek2007@gmail.com>
2021-01-16 16:04:55 -0500

    Fixed sync and api uri

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,8 @@
 let BACKEND_URI = "https://disinfodex-backend-production-e2kyhghera-ue.a.run.app"
-let SYNC_URI = ""
+let SYNC_URI = "https://disinfodex-sync-production-e2kyhghera-ue.a.run.app"
 
-BACKEND_URI = "http://localhost:5000"
-SYNC_URI = "http://localhost:3020"
+// BACKEND_URI = "http://localhost:5000"
+// SYNC_URI = "http://localhost:3020"
 
 module.exports = {
     BACKEND_URI,
