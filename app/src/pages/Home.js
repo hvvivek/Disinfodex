70018a4c80c00a321a6a2d436b08e5d6f0b33ba8
hvvivek <hvvivek2007@gmail.com>
2021-04-20 01:56:58 -0400

    added a loading screen

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,29 +1,49 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 
 import NetworksTable from "../components/NetworksTable"
 import NetworksCards from "../components/NetworksCards"
 import "../assets/stylesheets/style.css"
-
+import DataContext from "../contexts/DataContext"
 import SwitchToggler from "../components/SwitchToggler"
+import ScaleLoader from "react-spinners/ScaleLoader";
+import { css } from "@emotion/core";
+
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+`;
 
 function Home(props)
 {
     const [mode, setMode] = useState("table")
-    
+    let data = useContext(DataContext)
+    console.log(data)
+
     return <div className="flex-container" style={{"flexDirection":"column"}}>
             <Header {...{active: "database"}}/>
 
             <div className="flex-9">
                 <SwitchToggler {...{"active": mode, setMode}}/>
 
-                {mode === "table" &&                
+                {(!data.networks || (data.networks.length === 0)) && 
+                                
+                    <ScaleLoader
+                        css={override}
+                        size={150}
+                        color={"#003358"}
+                        loading={true}
+                    />
+                }
+
+                {data.networks && data.networks.length > 0 && mode === "table" &&                
                 <NetworksTable {...props.data}></NetworksTable>}
 
-                {mode === "cards" &&                
+                {data.networks && data.networks.length > 0 && mode === "cards" &&                
                 <NetworksCards {...props.data}></NetworksCards>}
-
             </div>
 
             <Footer {...{active: "database"}}/>
