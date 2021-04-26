5c1a62c0ad7b18a9c19a56baa9b78b24d7de698d
hvvivek <hvvivek2007@gmail.com>
2021-04-25 20:14:04 -0400

    Added resizable columns

--- app/src/components/NetworksTable.js
+++ app/src/components/NetworksTable.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useTable, usePagination, useFilters, useFlexLayout, useSortBy, useGlobalFilter } from 'react-table'
+import { useTable, usePagination, useFilters, useSortBy, useGlobalFilter, useResizeColumns, useBlockLayout } from 'react-table'
 import {NetworkTableFilters, GlobalFilter, exists, betweenDates, inArray} from './NetworkTableFilters'
 import NetworkTablePagination from './NetworkTablePagination'
 import { Modal } from 'react-bootstrap'
@@ -24,6 +24,7 @@ function NetworksTable(props)
       setCurrentNetwork(row_data)
       setModelOpen(true)
     }
+
     
     const tableInstance = useTable(
       {
@@ -40,8 +41,9 @@ function NetworksTable(props)
       useGlobalFilter,
       useFilters, 
       useSortBy,
-      useFlexLayout, 
-      usePagination
+      useBlockLayout, 
+      usePagination,
+      useResizeColumns
     )
     
     const {
@@ -54,7 +56,6 @@ function NetworksTable(props)
         state: { pageIndex, pageSize },
       } = tableInstance
 
-      
 
     return (
         <>
@@ -69,16 +70,33 @@ function NetworksTable(props)
                 {// Loop over the header rows
                     headerGroups.map(headerGroup => (
                         // Apply the header row props
-                        <div {...headerGroup.getHeaderGroupProps()} className="table-header-row">
+                        <div {...{...headerGroup.getHeaderGroupProps(), style: {"width":"max-content", "display":"flex"}}} className="table-header-row">
                         {// Loop over the headers in each row
                         headerGroup.headers.map(column => {
-                          // console.log(column)
+                          // let width = column.getHeaderProps(column.getSortByToggleProps()).style.width
+                          
+                          // console.log(column.getHeaderProps(column.getSortByToggleProps()))
+                          let headerProps = column.getHeaderProps(column.getSortByToggleProps())
+                          let propWidth = headerProps.style.width
+                          let width = propWidth
+
+                          // let width = widths[headerProps.key.split("_").pop()] * 0.9 * window.innerWidth
+                          // console.log(width)
+                          // console.log(widths)
+                          // console.log(0.9* window.innerWidth)
+                          // console.log(headerProps)
 
                           return (
                             // Apply the header cell props
-                            <div {...column.getHeaderProps(column.getSortByToggleProps())} className={"table-header-cell " + column.Header}>
+                            <div {...{...headerProps, style: {"width":width, "display":"inline-block", "boxSizing":"border-box", "position":"relative"}}} className={"table-header-cell " + column.Header}>
                             {// Render the header
                             column.render('Header')}
+                            <div
+                      {...column.getResizerProps()}
+                      className={`resizer ${
+                        column.isResizing ? 'isResizing' : ''
+                      }`}
+                    />
                             <span className={column.isSorted? "active": ""}>
                               {column.isSorted
                                 ? column.isSortedDesc
@@ -98,14 +116,21 @@ function NetworksTable(props)
                 page.map(row => {
                 // Prepare the row for display
                 prepareRow(row)
+                // console.log(row.getRowProps())
                 return (
                     // Apply the row props
-                    <div {...row.getRowProps()} className="table-body-row" onClick={() => openNetworkCard(row.original)}>
+                    <div {...{...row.getRowProps(), style: {"width":"max-content", "display":"flex"}}} className="table-body-row" onClick={() => openNetworkCard(row.original)}>
                     {// Loop over the rows cells
                     row.cells.map(cell => {
                         // Apply the cell props
+                        // console.log({...cell.getCellProps(), style: {"width":"xx"}})
+                        // let width = cell.getCellProps().style.width
+                        let cellProps = cell.getCellProps()
+                        let propWidth = cellProps.style.width
+                        let width = propWidth
+
                         return (
-                        <div {...cell.getCellProps()} className={"table-body-cell " + cell.column.Header}>
+                        <div {...{...cell.getCellProps(), style: {"width":width, "display":"inline-block", "boxSizing":"border-box", "position":"relative"}}} className={"table-body-cell " + cell.column.Header}>
                             {// Render the cell contents
                             cell.render('Cell')}
                         </div>
--- app/src/config/TABLE_VIEW.js
+++ app/src/config/TABLE_VIEW.js
@@ -5,20 +5,47 @@ import CellOriginCountry from "../components/CellRenderers/CellOriginCountry"
 import CellTargetCountry from "../components/CellRenderers/CellTargetCountry"
 import { BooleanColumnFilter, DateColumnFilter, SelectColumnFilter } from "../components/NetworkTableFilters"
 
+
+const widths = {
+    "Name"                      :0.10 * 0.9 * window.innerWidth,
+    "Dates"                     :0.10 * 0.9 * window.innerWidth,
+    "Company Unique"            :0.13 * 0.9 * window.innerWidth,
+    "Removal Type"              :0.15 * 0.9 * window.innerWidth,
+    "Named Entities"            :0.22 * 0.9 * window.innerWidth,
+    "Origin Countries Tagged"   :0.15 * 0.9 * window.innerWidth,
+    "Target Countries Tagged"   :0.15 * 0.9 * window.innerWidth
+}
+
+const minWidths = {
+    "Name"                      :150,
+    "Dates"                     :150,
+    "Company Unique"            :200,
+    "Removal Type"              :225,
+    "Named Entities"            :300,
+    "Origin Countries Tagged"   :225,
+    "Target Countries Tagged"   :225
+}
+
+
 const COLUMNS = [
         {
             Header          : "Network",
             accessor        : "Name",
             className       : 'network',
             disableFilters  : true,
-            // disableSortBy   : true
+            disableSortBy   : true,
+            width           : widths["Name"],
+            minWidth           : minWidths["Name"],
+            
         },
         {
             Header          : "Disclosure Date",
             accessor        : "Dates",
             Filter          : DateColumnFilter,
             filter          : 'betweenDates',
-            Cell            : CellDisclosureDate
+            Cell            : CellDisclosureDate,
+            width           : widths["Dates"],
+            minWidth           : minWidths["Dates"]
         },
         {
             Header          : "Source",
@@ -26,7 +53,9 @@ const COLUMNS = [
             Filter          : SelectColumnFilter,
             filter          : 'inArray',
             Cell            : CellSource,
-            disableSortBy   : true
+            disableSortBy   : true,
+            width           : widths["Company Unique"],
+            minWidth           : minWidths["Company Unique"]
 
         },
         {
@@ -35,7 +64,9 @@ const COLUMNS = [
             Filter          : SelectColumnFilter,
             filter          : 'inArray',
             Cell            : props => { return <div>{props.value && props.value.toString()}</div> },
-            disableSortBy   : true
+            disableSortBy   : true,
+            width           : widths["Removal Type"],
+            minWidth           : minWidths["Removal Type"]
 
         },
         {
@@ -44,7 +75,9 @@ const COLUMNS = [
             Filter          : SelectColumnFilter,
             filter          : 'inArray',
             Cell            : CellNamedEntities,
-            disableSortBy   : true
+            disableSortBy   : true,
+            width           : widths["Named Entities"],
+            minWidth           : minWidths["Named Entities"]
 
         },
         {
@@ -53,7 +86,9 @@ const COLUMNS = [
             Filter          : SelectColumnFilter,
             filter          : 'inArray',
             Cell            : CellOriginCountry,
-            disableSortBy   : true
+            disableSortBy   : true,
+            width           : widths["Origin Countries Tagged"],
+            minWidth           : minWidths["Origin Countries Tagged"]
 
         },
         {
@@ -62,7 +97,9 @@ const COLUMNS = [
             Filter          : SelectColumnFilter,
             filter          : 'inArray',
             Cell            : CellTargetCountry,
-            disableSortBy   : true
+            disableSortBy   : true,
+            width           : widths["Target Countries Tagged"],
+            minWidth           : minWidths["Target Countries Tagged"]
 
         },
         {
